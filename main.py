833d5fe52ccf2f46f9fa6845f433c9bcad67d4aa
memoryspeak <iliya.abdrakhimov@gmail.com>
2024-02-18 06:50:35 +0500

    bug fix

--- main.py
+++ main.py
@@ -47,56 +47,57 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill((0, 0, 0))
-    screen.blit(bg, (0, 0))
-
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_p]:
+        screen.fill((0, 0, 0))
         screen.blit(bg, (0, 0))
-    
+
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_a]:
-            if player_rect.left > 0:
-                player_rect.x -= player_speed
-        if keys[pygame.K_d]:
-            if player_rect.right < 800:
-                player_rect.x += player_speed
-        if keys[pygame.K_w]:
-            if player_rect.top > 0:
-                player_rect.y -= player_speed
-        if keys[pygame.K_s]:
-            if player_rect.bottom < 600:
-                player_rect.y += player_speed
+        if keys[pygame.K_p]:
+            screen.blit(bg, (0, 0))
         
-        if player_rect.colliderect(spike_rect):
-            text = font.render('Game Over ', True, (200, 0, 0))
-            text_rect = text.get_rect()
-            text_rect.center = (400, 300)
-            screen.blit(text, text_rect)
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_a]:
+                if player_rect.left > 0:
+                    player_rect.x -= player_speed
+            if keys[pygame.K_d]:
+                if player_rect.right < 800:
+                    player_rect.x += player_speed
+            if keys[pygame.K_w]:
+                if player_rect.top > 0:
+                    player_rect.y -= player_speed
+            if keys[pygame.K_s]:
+                if player_rect.bottom < 600:
+                    player_rect.y += player_speed
+            
+            if player_rect.colliderect(spike_rect):
+                text = font.render('Game Over ', True, (200, 0, 0))
+                text_rect = text.get_rect()
+                text_rect.center = (400, 300)
+                screen.blit(text, text_rect)
+                pygame.display.update()
+                pygame.time.delay(5000)
+                running = False
+            
+            if player_rect.colliderect(pomegranate_rect):
+                player_score += 1
+                pomegranate_rect.center = get_random_position()
+                spike_speed += 0.1
+            
+            if spike_rect.left < 0 or spike_rect.right > 800:
+                spike_direction_x = -spike_direction_x
+            if spike_rect.top < 0 or spike_rect.bottom > 600:
+                spike_direction_y = -spike_direction_y
+
+            spike_rect.x += spike_speed * spike_direction_x
+            spike_rect.y += spike_speed * spike_direction_y
+
+            screen.blit(spike_img, spike_rect)
+            screen.blit(player_img, player_rect)
+            screen.blit(pomegranate_img, pomegranate_rect)
+
+            score_text = font.render(f'Очки: {player_score}', True, (255, 255, 255))
+            screen.blit(score_text, (10, 10))
+            
             pygame.display.update()
-            pygame.time.delay(5000)
-            running = False
-        
-        if player_rect.colliderect(pomegranate_rect):
-            player_score += 1
-            pomegranate_rect.center = get_random_position()
-            spike_speed += 0.1
-        
-        if spike_rect.left < 0 or spike_rect.right > 800:
-            spike_direction_x = -spike_direction_x
-        if spike_rect.top < 0 or spike_rect.bottom > 600:
-            spike_direction_y = -spike_direction_y
-
-        spike_rect.x += spike_speed * spike_direction_x
-        spike_rect.y += spike_speed * spike_direction_y
-
-        screen.blit(spike_img, spike_rect)
-        screen.blit(player_img, player_rect)
-        screen.blit(pomegranate_img, pomegranate_rect)
-
-        score_text = font.render(f'Очки: {player_score}', True, (255, 255, 255))
-        screen.blit(score_text, (10, 10))
-        
-        pygame.display.update()
-        clock.tick(fps)
-    pygame.quit()
+            clock.tick(fps)
+pygame.quit()
+exit()
